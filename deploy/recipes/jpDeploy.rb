a0330d86d73658ca74f9606b94238af3ef944038
Monil <30290753+mp-dasglobtech@users.noreply.github.com>
2019-08-07 16:26:11 +0530

    Update jpDeploy.rb
    Added kill process to port 80

--- deploy/recipes/jpDeploy.rb
+++ deploy/recipes/jpDeploy.rb
@@ -3,6 +3,10 @@ Chef::Log.info("Ready to Deploy")
 Chef::Log.info(node[:deploy])
 
 node[:deploy].each do |current_path, deploy|
+  
+  Chef::Log.info("Kill already allocated port");  
+  execute("sudo fuser -k 80/tcp")
+  
   Chef::Log.info("npm start --prefix #{deploy[:current_path]}");
 #   Remove this tag comment in when we are going to user npm start.
   execute("sudo npm start --prefix #{deploy[:current_path]} &")
