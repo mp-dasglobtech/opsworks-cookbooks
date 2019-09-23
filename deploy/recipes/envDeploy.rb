9e07c0a81c440cdb2199b00029c03b312e8c57b2
Monil <30290753+mp-dasglobtech@users.noreply.github.com>
2019-09-23 12:27:23 +0530

    Update envDeploy.rb
    Corrected syntax

--- deploy/recipes/envDeploy.rb
+++ deploy/recipes/envDeploy.rb
@@ -2,10 +2,10 @@ Chef::Log.info("Setting Environment Variables")
 node[:deploy].each do |current_path, deploy, environment_variables|
   Chef::Log.info(deploy[:environment_variables])
   deploy[:environment_variables].each do |name, value|
-    ENV["#{name}"] = "#{value}
+    ENV["#{name}"] = "#{value}"
   end
 
-  Chef::Log.inof("Writing variables to etc/environment to have them after restart")
+  Chef::Log.info("Writing variables to etc/environment to have them after restart")
   template "#{deploy[:current_path]}/.env" do
     source "environment.erb"
     mode "0644"
